6201fbea27067fc72c0405f1a4263c857a107126
dizhengcindy <dizhengcindy@gmail.com>
2020-03-12 21:41:28 -0700

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,23 @@
+class Appointment
+    attr_reader :name, :patient, :doctor
+    @@all =[]
+
+        def initialize(date,patient,doctor)
+            @date = date
+            @patient = patient
+            @doctor = doctor
+            @@all << self
+        end
+    
+        def self.all
+            @@all
+        end
+        
+        def patient
+            @patient
+        end
+        
+        def doctor
+            @doctor
+        end
+end
