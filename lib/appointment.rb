c2808d76d15a06f95e7e22ffafc6de86cedd2761
christine <bumpouce@gmail.com>
2020-03-19 08:45:15 -0700

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Appointment
+
+    attr_accessor :date
+    attr_reader :doctor, :patient
+
+    @@all = []
+
+    def initialize (date, patient, doctor)
+        @date = date
+        @doctor = doctor
+        @patient = patient
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
